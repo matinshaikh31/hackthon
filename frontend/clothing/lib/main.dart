79be28b58e92c301a11a2c8a12695439742871dd
MetaDC <150433207+MetaDC@users.noreply.github.com>
2025-07-12 11:03:51 +0530

    main

--- frontend/clothing/lib/main.dart
+++ frontend/clothing/lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:ui';
+
 import 'package:clothing/firebase_options.dart';
+import 'package:clothing/shared/router.dart';
+import 'package:clothing/shared/theme.dart';
 import 'package:clothing/views/home/homepage.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -15,27 +19,25 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: Homepage(),
+    // print(countryCodeFlagsList.map((e) => e['flag']));
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      title: 'ReWear',
+      routerConfig: appRouter,
+      theme: themeData,
+      scrollBehavior: MyCustomScrollBehavior(),
     );
   }
+}
+
+class MyCustomScrollBehavior extends MaterialScrollBehavior {
+  // Override behavior methods and getters like dragDevices
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+    PointerDeviceKind.mouse,
+    PointerDeviceKind.touch,
+    PointerDeviceKind.stylus,
+    PointerDeviceKind.unknown,
+    PointerDeviceKind.trackpad,
+  };
 }
